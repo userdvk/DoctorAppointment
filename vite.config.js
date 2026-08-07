95eaf80509355e9d9af4af2193d2b2fc9e4be9bc
userdvk <vladyslav.kurilov@gmail.com>
2026-08-07 19:43:41 +0100

    Fix Manifest

--- vite.config.js
+++ vite.config.js
@@ -12,18 +12,7 @@ export default defineConfig({
         name: "DoctorApp",
         short_name: "Doctor Appointmets ",
         theme_color: "#000000",
-        icons: [
-          {
-            src: "/logo192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/logo512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+        icons: [],
       },
     }),
   ],
