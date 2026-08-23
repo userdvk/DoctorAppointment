62144dedf8c2cf830da0a38b0cb5d675a146c807
userdvk <vladyslav.kurilov@gmail.com>
2026-08-23 20:05:38 +0100

    content in full doctor page

--- src/pages/AppointmentDetailes/AppointmentDetailes.jsx
+++ src/pages/AppointmentDetailes/AppointmentDetailes.jsx
@@ -1,14 +1,46 @@
 import { useParams } from "react-router-dom";
 import { PageContainer } from "../../components/PageContainer/PageContainer";
 import useDoctors from "../../hooks/useDoctors";
+import cls from "./AppointmentDetailes.module.css";
+import { IoChatbubbleOutline } from "react-icons/io5";
 
 export default function AppointmentDetailes() {
   const { id } = useParams();
 
-  const [doctors] = useDoctors();
+  const [doctors, loading] = useDoctors();
 
   const doctor = doctors.find((item) => item.id === id);
   console.log(doctor);
 
-  return <PageContainer title={"Appointment"} showBackButton></PageContainer>;
+  if (loading) {
+    return (
+      <PageContainer title={"Appointment"} showBackButton>
+        <div>Загрузка...</div>
+      </PageContainer>
+    );
+  }
+
+  return (
+    <PageContainer title={"Appointment"} showBackButton>
+      <div className={cls.header}>
+        <div className={cls.photo}>
+          <img src={`/${doctor.photo}`} />
+        </div>
+
+        <div className={cls.nameWrap}>
+          <div className={cls.name}>
+            {doctor.name}
+            <div className={cls.chatBtn}>{<IoChatbubbleOutline />}</div>
+          </div>
+
+          <span className={cls.status}>denteeth</span>
+
+          <div className={cls.payment}>
+            <div>Payment</div>
+            <div>$120.00</div>
+          </div>
+        </div>
+      </div>
+    </PageContainer>
+  );
 }
