5c5fe92afb128634e9ebe623f3115e807ceb0eb7
userdvk <vladyslav.kurilov@gmail.com>
2026-08-30 15:08:41 +0100

    calendar

--- src/components/features/AppCalendar/AppCalendar.jsx
+++ src/components/features/AppCalendar/AppCalendar.jsx
@@ -1,10 +1,53 @@
+import { useContext, useState } from "react";
+import { Button } from "../../ui/Button/Button";
 import cls from "./AppCalendar.module.css";
 import Calendar from "react-calendar";
+import { AuthContext } from "../../../context/AuthContext";
+import { arrayUnion, doc, setDoc } from "firebase/firestore";
+import { db } from "../../../firebase";
+import { useParams } from "react-router-dom";
 
 export function AppCalendar() {
+  const user = useContext(AuthContext);
+  const { id } = useParams();
+
+  const [pickDate, setDate] = useState(null);
+
+  const setAppointment = async () => {
+    if (!user) return;
+
+    try {
+      await setDoc(
+        doc(db, "doctorsList", id),
+        {
+          appointments: arrayUnion({
+            pickDate: pickDate,
+            userId: user.uid,
+            user: user.name,
+          }),
+        },
+        { merge: true },
+      );
+
+      console.log("Has been updated");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className={cls.calendarWrap}>
-      <Calendar />
-    </div>
+    <>
+      <div className={cls.calendarWrap}>
+        <Calendar
+          view="month"
+          minDetail="month"
+          maxDetail="month"
+          onChange={(date) => setDate(date)}
+          value={pickDate}
+        />
+      </div>
+
+      <Button onClick={setAppointment}>Set Appointment</Button>
+    </>
   );
 }
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,9 +1,9 @@
 import cls from "./Button.module.css";
 
-export function Button({ children }) {
+export function Button({ children, onClick }) {
   return (
     <div className={cls.btnGroup}>
-      <button className={cls.btn} type="submit">
+      <button className={cls.btn} type="submit" onClick={onClick}>
         {children}
       </button>
     </div>
